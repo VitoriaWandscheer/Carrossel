40c153ec44c9e53064b08e901fd19b0304a572fa
Vitória <vitoriawpereira2404@gmail.com>
2023-07-26 17:04:24 -0300

    Create const in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const slideWrapper = document.querySelector('[data-slide="wrapper"]')
+const slideList = document.querySelector('[data-slide="list"]')
+const navPreviousButton = document.querySelector('[data-slide="nav-previous-button"]')
+const navNextButton = document.querySelector('[data-slide="nav-next-button"]')
+const slideControlsWrapper = document.querySelector('[data-slide="controls-wrapper"]')
+const slideControlButtons = document.querySelectorAll('[data-slide="control-button"]')
+const slideItems = document.querySelectorAll('[data-slide="item"]')
