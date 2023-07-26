6b8886ce87782abd5730e286bb9e0442b386498f
Vitória <vitoriawpereira2404@gmail.com>
2023-07-26 18:00:22 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,27 @@ const navNextButton = document.querySelector('[data-slide="nav-next-button"]')
 const slideControlsWrapper = document.querySelector('[data-slide="controls-wrapper"]')
 const slideControlButtons = document.querySelectorAll('[data-slide="control-button"]')
 const slideItems = document.querySelectorAll('[data-slide="item"]')
+
+function onMouseDown(event) {
+    const slideItem = event.currentTarget
+    slideItem.addEventListener('mousemove', onMouseMove)
+    console.log('Apertei o botão do mouse.')
+}
+
+function onMouseMove(event) {
+    console.log('Movimentei o mouse em cima do elemento.')
+}
+
+function onMouseUp(event) {
+    const slideItem = event.currentTarget
+    slideItem.removeEventListener('mousemove', onMouseMove)
+    console.log('Soltei o botão do mouse.')
+}
+
+slideItems.forEach(function(slideItem, index) {
+    slideItem.addEventListener('dragstart', function(event) {
+        event.preventDefault()
+    })
+    slideItem.addEventListener('mousedown', onMouseDown)
+    slideItem.addEventListener('mouseup', onMouseUp)
+})
