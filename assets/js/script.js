3971cf1ea8a3cdba0e44a5f2ee3cd625d23184ab
Vitória <vitoriawpereira2404@gmail.com>
2023-08-11 18:01:17 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,32 +7,47 @@ const navNextButton = document.querySelector('[data-slide="nav-next-button"]')
 const slideControlsWrapper = document.querySelector('[data-slide="controls-wrapper"]')
 const slideControlButtons = document.querySelectorAll('[data-slide="control-button"]')
 const slideItems = document.querySelectorAll('[data-slide="item"]')
-let startPoint = 0
-let savedPosition = 0
-let currentPoint = 0
-let movement = 0
-let currentSlideIndex = 0
+
+const state = {
+    startPoint: 0,
+    savedPosition: 0,
+    currentPoint: 0,
+    movement: 0,
+    currentSlideIndex: 0
+}
 
 function onMouseDown(event, index) {
     const slideItem = event.currentTarget
-    startPoint = event.clientX
-    currentPoint = event.clientX - savedPosition
-    currentSlideIndex = index
+    state.startPoint = event.clientX
+    state.currentPoint = event.clientX - state.savedPosition
+    state.currentSlideIndex = index
+    console.log(state.currentSlideIndex)
     slideItem.addEventListener('mousemove', onMouseMove)
 }
-
 function onMouseMove(event) {
-    const movement = event.clientX - startPoint
-    const position = event.clientX - currentPoint
+    state.movement = event.clientX - state.startPoint
+    const position = event.clientX - state.currentPoint
     slideList.style.transform = 'translateX('+position+'px)'
-    savedPosition = position
+    state.savedPosition = position
 }
-
 function onMouseUp(event) {
     const slideItem = event.currentTarget
+    const slideWidht = slideItem.clientWidth
+    if (state.movement < -150) {
+        const position = (state.currentSlideIndex + 1) * slideWidht
+        slideList.style.transform = 'translateX('+(-position)+'px)'
+        state.savedPosition = -position
+    } else if (state.movement > 150) {
+        const position = (state.currentSlideIndex - 1) * slideWidht
+        slideList.style.transform = 'translateX('+(-position)+'px)'
+        state.savedPosition = -position
+    } else {
+        const position = (state.currentSlideIndex) * slideWidht
+        slideList.style.transform = 'translateX('+(-position)+'px)'
+        state.savedPosition = -position
+    }
     slideItem.removeEventListener('mousemove', onMouseMove)
 }
-
 slideItems.forEach(function(slideItem, index) {
     slideItem.addEventListener('dragstart', function(event) {
         event.preventDefault()
